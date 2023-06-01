5597eb9205c50a98a24bf0c3227234e08b66b70f
farbodhma <88799675+farbodhma@users.noreply.github.com>
2023-06-01 10:35:05 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,5 +1,22 @@
-module predictor(input wire request, result, clk, taken, output reg prediction);
+module predictor(
+  input wire request,  // Prediction request
+  input wire result,   // Result to predict
+  input wire clk,      // Clock
+  input wire taken,    // Whether the previous result was taken or not
+  output reg prediction // Predicted result
+);
 
-// Your code
+  reg prev_taken;      // Previous result
+
+  always @(posedge clk) begin
+    if (request) begin
+      if (prev_taken) begin
+        prediction <= 1; // Predict taken
+      end else begin
+        prediction <= 0; // Predict not taken
+      end
+      prev_taken <= taken; // Store the current result
+    end
+  end
 
 endmodule
